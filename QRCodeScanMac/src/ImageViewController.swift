28214514d2e244751bca5e7767bb6199fb753835
Joe <sheninter@163.com>
2022-04-20 20:46:43 +0800

    窗口增加文字说明

--- QRCodeScanMac/src/ImageViewController.swift
+++ QRCodeScanMac/src/ImageViewController.swift
@@ -9,15 +9,34 @@ import Cocoa
 
 class ImageViewController: NSViewController, ImageCanvsDelegate {
 
+    @IBOutlet weak var accessoryLabel: NSTextField!
+    
+    @IBOutlet weak var imageCanvas: ImageCanvas!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.accessoryLabel.isEditable = false
+        self.accessoryLabel.isSelectable = false
+        self.accessoryLabel.placeholderString = "拖拽二维码图片，打开浏览器访问二维码关联地址。"
     }
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
+        }
+    }
+    
+    // 鼠标左击
+    override func mouseUp(with event: NSEvent) {
+        print("\(#fileID) \(#function) \(#line).")
+        
+        if self.imageCanvas.image == nil {
+            // TODO: 显示文件选择窗口
+        } else {
+            self.imageCanvas.image = nil
+            self.accessoryLabel.isHidden = false
         }
     }
 
@@ -45,8 +64,10 @@ class ImageViewController: NSViewController, ImageCanvsDelegate {
     // MARK: - ImageCanvsDelegate
     func draggingEnd(_ imageCanvas: ImageCanvas, sender: NSDraggingInfo) {
         guard (imageCanvas.image != nil) else {
+            self.accessoryLabel.isHidden = false
             return
         }
+        self.accessoryLabel.isHidden = true
         
         guard let string = self.decterQRCode(imageCanvas.image!) else {
             return
